c4a687122afba4d7307f983e345861e8eb5464cd
homomaid <canon.xex@gmail.com>
2017-11-17 10:06:06 +0900

    出力形式を変更

--- binpacking.py
+++ binpacking.py
@@ -5,7 +5,6 @@ import time
 import random
 import argparse
 
-
 def binpacking_dual(B, s, a):
     problem = pulp.LpProblem('binpacking-dual', pulp.LpMaximize)
 
@@ -16,22 +15,17 @@ def binpacking_dual(B, s, a):
 
     c = np.ones((1, len(s)))
     cy = c * y
-
     problem += cy[0, 0]
 
     A = np.asarray(np.identity(len(s)))
-
     if a != [[]]:
         A = np.append(A, a, axis=0)
     A = np.asmatrix(A)
-
     Ay = A * y
-
     for i in Ay:
         problem += i[0, 0] <= 1
 
     problem.solve()
-
     ret = np.array([])
     for i in y:
         ret = np.append(ret, i[0, 0].value())
@@ -47,16 +41,13 @@ def zero_one_knapsack(B, s, y):
     a = np.asmatrix(a.reshape(len(s), 1))
 
     ya = y * a
-
     problem += ya[0, 0]
 
     s = np.append([], s).reshape(1, len(s))
     sa = s * a
-
     problem += sa[0, 0] <= B
 
     problem.solve()
-
     ret = np.array([])
     for i in a:
         ret = np.append(ret, i[0, 0].value())
@@ -73,7 +64,6 @@ def binpacking(a):
 
     c = np.ones((1, a[0].size))
     cx = c * x
-
     problem += cx[0, 0]
 
     Ax = a * x
@@ -81,58 +71,44 @@ def binpacking(a):
         problem += i[0, 0] >= 1
 
     problem.solve()
-
     ret = np.array([])
     for i in x:
         ret = np.append(ret, i[0, 0].value())
 
     return np.asmatrix(ret.reshape(1, a[0].size))
 
-parser = argparse.ArgumentParser(description="hogehoge")
-parser.add_argument("--size", "-s", type=int, required=True)
-parser.add_argument("--nitem", "-n", type=int, required=True)
-parser.add_argument("--min", "-m", type=int, required=True)
-args = parser.parse_args()
-
 start = time.time()
-B = args.size
-s = []
-for i in range(0, args.nitem):
-    s.append(random.randint(args.min, B - 1))
+B = 8
+s = [3,3,3]
 
 a = np.array([[]])
+
+print("## Parameter:")
 print("Bin size: " + str(B))
 print("Item list: " + str(s))
-i = 1
+print("\n## Dual Problem:")
 
+i = 1
 while True:
-    print("### TEMP-" + str(i))
     y = binpacking_dual(B, s, a)
     mat = zero_one_knapsack(B, s, y)
+
+    print("### TEMP-" + str(i))
     print("binpacking: " + str(y))
     print("0-1 knapsack: " + str(mat))
 
     if (mat * y.T)[0, 0] > 1:
-        if i == 1:
-            a = np.array(mat)
-        else:
-            a = np.append(a, mat, axis=0)
+        a = np.array(mat) if i == 1 else np.append(a, mat, axis=0)
     else:
         break
 
-    print("constraint: ")
-    print(str(np.append(np.identity(len(s)), a, axis=0).T) + "\n")
-
     i += 1
 
-print("\n主問題: ")
+constraint = np.identity(len(s)).T if a.size == 0 else np.append(np.identity(len(s)), a, axis=0).T
+
+print("\n## Main Problem:")
 print("constraint: ")
-if a.size == 0:
-    constraint = np.identity(len(s))
-else:
-    constraint = np.append(np.identity(len(s)), a, axis=0).T
 print(constraint)
 print(binpacking(constraint))
-
-print("\n実行回数: " + str(i))
-print("実行時間: " + str(time.time() - start))
+print("Count: " + str(i))
+print("Time: " + str(time.time() - start))
