85aa9ac23eb6c859bd22fe9d7e51edb0c8525c7b
homomaid <canon.xex@gmail.com>
2017-12-04 11:32:22 +0900

    command 変数の内容を変更

--- main.fish
+++ main.fish
@@ -3,7 +3,7 @@ set n_max 1000
 set dis 2
 
 for n in (seq -w 880 10 $n_max)
-    set command python main.py -n $n -b $bin_size -d $dis
+    set command python main.py -n $n -b $bin_size -d $dis | tee /dev/stderr | tail -n 1 >> output2.txt
     echo $command  
-    eval $command | tee /dev/stderr | tail -n 1 >> output2.txt
+    eval $command
 end
