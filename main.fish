88d140842f87c341cfeffc3d7bd17cb8a044ffc8
homomaid <canon.xex@gmail.com>
2017-12-08 15:24:00 +0900

    ログに書き込まれない問題を修正（Fish Script をちゃんと使おうね ^^;）

--- main.fish
+++ main.fish
@@ -1,9 +1,7 @@
 set bin_size 10
 set n_max 1000
-set dis 2
+set dis 1
 
-for n in (seq -w 880 10 $n_max)
-    set command python main.py -n $n -b $bin_size -d $dis | tee /dev/stderr | tail -n 1 >> output2.txt
-    echo $command  
-    eval $command
+for n in (seq -w 500 10 $n_max)
+    python main.py -n $n -b $bin_size -d $dis | tee /dev/stderr | tail -n 1 >> output.txt
 end
