f9818283601ddae0e6181c73c0ddea6a5c9b81c7
homomaid <canon.xex@gmail.com>
2017-12-18 18:44:32 +0900

    Delete fish script and update shell script

--- main.fish
+++
@@ -1,7 +0,0 @@
-set bin_size 10
-set n_max 5000
-set dis 9
-
-for n in (seq -w 100 100 $n_max)
-    python main.py -n $n -b $bin_size -d $dis | tee /dev/stderr | tail -n 1 >> output9.txt
-end
--- main.sh
+++ main.sh
@@ -1,7 +1,23 @@
-bin_size=10
-n_max=20
-dis=1
+exec_python () {
+    bin_size=$1
+    n_min=$2
+    n_max=$3
+    dis=$4
 
-for n in `seq -w 10 1 $n_max`; do
-    python ./main.py -n $n -b $bin_size -d $dis | tee /dev/stderr | tail -n 1 >> output.txt
-done
+    for n in `seq -w $n_min 100 $n_max`; do
+        python main.py -n $n -b $bin_size -d $dis >> output$dis.txt
+    done
+}
+
+exec_python 10 700 5000 1 &
+exec_python 10 1300 5000 2 &
+exec_python 10 1300 5000 4 &
+exec_python 10 1600 5000 5 &
+exec_python 10 1000 5000 6 &
+exec_python 10 1000 5000 7 &
+exec_python 10 500 5000 8 &
+exec_python 10 300 5000 9 &
+exec_python 10 1300 5000 10 &
+exec_python 10 1000 5000 11 &
+exec_python 10 500 5000 12 &
+exec_python 10 300 5000 13 &
